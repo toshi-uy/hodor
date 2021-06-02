0b1d31ce845934a06b607c934d34ca566d762519
toshi-uy <s_borgia@hotmail.com>
2021-06-02 14:07:40 -0300

    changed page to level_1

--- level_1/level_1.py
+++ level_1/level_1.py
@@ -5,7 +5,7 @@ import re
 import mechanize
 
 br = mechanize.Browser()
-br.open("http://158.69.76.135/level0.php")
+br.open("http://158.69.76.135/level1.php")
 for i in range(4096):
     br.select_form(nr=0)
     br["id"] = "2785"
