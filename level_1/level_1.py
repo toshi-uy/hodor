d997572cd12dfb89cd0d586d918276ba3b002dce
toshi-uy <s_borgia@hotmail.com>
2021-06-02 14:04:43 -0300

    added level_1 and put back re module

--- level_1/level_1.py
+++ level_1/level_1.py
@@ -4,13 +4,10 @@
 import re
 import mechanize
 
-for i in range(1024):
-    br = mechanize.Browser()
-    br.open("http://158.69.76.135/level0.php")
+br = mechanize.Browser()
+br.open("http://158.69.76.135/level0.php")
+for i in range(4096):
     br.select_form(nr=0)
     br["id"] = "2785"
     response1 = br.submit()
-    response2 = br.back()
-    response2.get_data()
-    response4 = br.reload()
 print("Done!")
